1aa185049489757dce26e80173366e3486e8d63d
shaqua <shaqua@localhost.localdomain>
2020-08-10 10:24:26 -0400

    final version

--- vcs_revision.py
+++ vcs_revision.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import re, json, paramiko, sys
+import re, json, paramiko, sys, time
 file = sys.argv[1]
 f = open(file, "r")
 connections = []
@@ -8,25 +8,33 @@ conn_params = re.sub(r'\[|\]', '', f.read().replace('},', '};').strip()).split('
 for host in conn_params:
     connections.append(json.loads(host))
 for connection in connections:
+  auth_method = "certificate"
   client = paramiko.SSHClient()
   client.load_system_host_keys()
   client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-  client.connect(connection["hostname"], username=connection["user"], password=connection["user"])
+  try:
+        client.connect(connection["hostname"], username=connection["user"])
+  except paramiko.ssh_exception.AuthenticationException:
+        client.connect(connection["hostname"], username=connection["user"], password=connection["user"])
+        auth_method = "password"
   ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command("cd bw/ && git branch")
-  if (ssh_stderr is None):
+  if (ssh_stderr.readline().strip()==''):
      vcs_type = "git"
-     vcs_branch = ssh_stdout.read()
+     vcs_branch = ssh_stdout.readline().strip()
      ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command("cd bw/ && git log | head -1 | awk '{print $0}'")
-     vcs_revision = ssh_stdout.read()
+     vcs_revision = ssh_stdout.readline().strip()
   else:
     vcs_type = "svn"
     ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command("cd bw/ && svn info | grep '^URL:' | egrep -o '(tags|branches)/[^/]+|trunk' | egrep -o '[^/]+$'")
-    vcs_branch = ssh_stdout.read()
-    ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command("cd bw/ && svn info | grep Revision | awk '{print $1}'")
-    vcs_revision = ssh_stdout.read()
-  if (client.get_host_keys() is None):
-    auth_method = "password"
-  else:
-    auth_method = "certificate"
+    vcs_branch = ssh_stdout.readline().strip()
+    ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command("cd bw/ && svn info | grep Revision | awk '{print $2}'")
+    vcs_revision = ssh_stdout.readline().strip()
+#  if (client.get_host_keys() is None):
+#    auth_method = "password"
+#  else:
+#    auth_method = "certificate"
   results.append({connection["user"]:{"vcs_type":vcs_type, "branch":vcs_branch, "revision":vcs_revision, "auth_type":auth_method}})
-print(json.dumps(results, sort_keys=True))
+  client.close()
+f.close()
+time.sleep(5)
+print(json.dumps(results, indent=4, sort_keys=False))
